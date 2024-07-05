062f8ed8aa3a7458e8c195be9b23720fd9abd055
sibeni-li <pro@sibeni.li>
2024-07-05 21:00:33 +0200

    Intégration de la page  en utilisant les composants  et  et en réutilisant le composant  avec les props + Ajout dy style des collapses

--- src/pages/Information/index.jsx
+++ src/pages/Information/index.jsx
@@ -1,10 +1,60 @@
 import Banner from "../../components/Banner"
+import Collapse from "../../components/Collapse"
 import imageInfo from "../../assets/imageInfo.png"
 import "../../styles/Information.scss"
 
+const aboutList = [
+    {
+        id:0 ,
+        title: 'Fiabilité',
+        content:
+        'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.',
+    },
+    {
+        id:1 ,
+        title: 'Respect',
+        content:
+        'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.',
+    },
+    {
+        id:2 ,
+        title: 'Service',
+        content:
+        "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+    },
+    {
+        id:3 ,
+        title: 'Sécurité',
+        content:
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+];
+
 function Information () {
+    //State
+
+    //Comportement
+    const about = aboutList.map((aboutList) => {
+        const idAbout = aboutList.id
+        return (
+            <Collapse 
+                key={idAbout}
+                id={idAbout}
+                titleCollapse={aboutList.title} 
+                contentCollapse={aboutList.content}
+            />
+        )
+    })
+    //Render
     return (
-        <Banner imageSrc={imageInfo}/>
+        <div>
+            <Banner 
+                imageSrc={imageInfo}
+            />
+            <div>
+                {about}
+            </div>
+        </div>
     )
 }
 
