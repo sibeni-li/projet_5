4c56288e888091ce1b714b32de29542417b4b55e
sibeni-li <pro@sibeni.li>
2024-07-05 20:54:20 +0200

    Ajout du composant Collapse + Création de sa feuille de style

---
+++ src/components/Collapse/index.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+import "../../styles/Collapse.scss"
+import arrowDown from "../../assets/arrowDown.png"
+import arrowUp from "../../assets/arrowUp.png"
+
+function Collapse ({titleCollapse , contentCollapse}) {
+    //State
+    const [openCollapse, setOpenCollapse] = useState(false)
+    console.log(openCollapse)
+    //Comportement
+    const handleClick = () => {
+        setOpenCollapse(!openCollapse)
+    }
+    //Render
+    if (openCollapse) {
+        return (
+            <div className="collapse">
+                <h2 className="collapse-title">
+                    {titleCollapse}
+                    <img src={arrowDown} alt="" onClick={handleClick}></img>
+                </h2>
+                <div className="collapse-content">{contentCollapse}</div>
+            </div>
+        ) 
+    } else {
+        return (
+            <div className="collapse">
+                <h2 className="collapse-title">
+                    {titleCollapse}
+                    <img src={arrowUp} alt="" onClick={handleClick}></img>
+                </h2>
+            </div>
+        )
+    }
+}
+
+export default Collapse
+
+/***
+ * ***TODO***
+ * Ajouter animation de transition à l'ouverture du collapse
+ * 
+ */
